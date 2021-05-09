f09fc3c76eeb19a787a43bfc294894759fe20dac
chulang <1350930355@qq.com>
2021-05-09 17:18:48 +0800

    test01

---
+++ spark-core/src/main/java/com/atguigu/bigdata/spark/core/test/SubTask.scala
@@ -0,0 +1,13 @@
+package com.atguigu.bigdata.spark.core.test
+
+/**
+ *
+ * @author chulang
+ * @date 2021/5/9
+ * @description TODO
+ */
+class SubTask extends Serializable {
+
+  var datas:List[Int] = _
+
+}
